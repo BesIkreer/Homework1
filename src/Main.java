7182f9815f024c843d090ab9edcdd2d6545c0a7d
Bez <besik.okey@gmail.com>
2022-12-24 12:55:23 +0300

    WorkHome.1 Работа с переменными

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        double dog1;
+
+
+        dog1 = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println(dog1);
+        System.out.println(cat);
+        System.out.println(paper);
+       }
     }
-}
