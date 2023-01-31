117ca5ce7ee499e1dd2b0a351a059a76ec5c2ff7
Bez <besik.okey@gmail.com>
2023-01-31 22:31:33 +0300

    WorkHome.6 Методы!!!

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,62 @@
+import java.time.LocalDate;
+
 public class Main {
+    public static void checkYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год - високосный год");
+        } else {
+            System.out.println(year + " год - невисокосный год");
+        }
+    }
+
+    public static void findVersion(int[] input) {
+        int inputYear = input[1];
+        int inputOs = input[0];
+        int clientDeviceYear = LocalDate.now().getYear();
+        int clientOs = 0;
+        if (inputOs == clientOs && inputYear >= clientDeviceYear) {
+            System.out.println("Установите версию приложения для IOS по ссылке.");
+        } else if (inputOs == clientOs && inputYear < clientDeviceYear) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке.");
+        } else if (inputOs != clientOs && inputYear >= clientDeviceYear) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (inputOs != clientOs && inputYear < clientDeviceYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }
+    }
+
+    public static int calculationDayDelivery(int inputDeliveryDistance) {
+        int dayDelivery = 0;
+        if (inputDeliveryDistance < 20) {
+            dayDelivery = 1;
+        } else if (inputDeliveryDistance >= 20 && inputDeliveryDistance < 60) {
+            dayDelivery = 2;
+        } else if (inputDeliveryDistance >= 60 && inputDeliveryDistance < 100) {
+            dayDelivery = 3;
+        } else {
+            dayDelivery = 0;
+        }
+        return dayDelivery;
+    }
+
+
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
-    }
-
-    public static void task1() {
-        System.out.println("Task 1");
-        String firstName = "Ivan ";
-        String middleName = "Ivanov ";
-        String lastName = "Ivanovich";
-        String fullName = middleName + firstName + lastName;
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
-    }
-
-    public static void task2() {
-        System.out.println("Task 2");
-        String firstName = "Ivan ";
-        String middleName = "Ivanov ";
-        String lastName = "Ivanovich";
-        String fullName = middleName + firstName + lastName;
-        System.out.println("Ф.И.О. сотрудника - " + fullName.toUpperCase());
-
-    }
-    public static void task3() {
-        System.out.println("Task 3");
-        String firstName = "Ivёn ";
-        String middleName = "Ivanov ";
-        String lastName = "Ivanovёch";
-        String fullName = middleName + firstName + lastName;
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println("Task1");
+        int yearIn = 2021;
+        checkYear(yearIn);
+        System.out.println("Task2");
+        int inputOs = 0;
+        int currentYear = 2023;
+        int[] input = new int[2];
+        input[0] = inputOs;
+        input[1] = currentYear;
+        findVersion(input);
+        System.out.println("Task3");
+        int inputDistance = 12;
+        int outDayDelivery = calculationDayDelivery(inputDistance);
+        if (outDayDelivery == 0) {
+            System.out.println("Доставка не возможна.");
+        }
+        System.out.println("Потребуется дней: " + outDayDelivery);
     }
 }
